cb12f038968dfe1ef58c51bd559912dcba8aae0d
Ravnoor Gill <ravnoor@gmail.com>
2021-02-27 23:40:51 -0500

    remove unused/commented code segments and hardcoded variables

--- app/utils/patch_dataloader.py
+++ app/utils/patch_dataloader.py
@@ -14,25 +14,17 @@ from utils.keras_bayes_utils import *
 
 def test_scan_uncertainty(model, test_x_data, scan, options, intermediate=None, save_nifti=False, uncertainty=True, candidate_mask=None, T=20):
     """
-    Test data based on one model
-    Input:
+    inference data based on a single model
+    inputs:
     - test_x_data: a nested dictionary containing training image paths:
             train_x_data['scan_name']['modality'] = path_to_image_modality
     - save_nifti: save image segmentation
     - candidate_mask: a binary masks containing voxels to classify
 
-    Output:
-    - test_scan = Output image containing the probability output segmentation
-    - If save_nifti --> Saves a nii file at specified location options['test_folder']/['test_scan']
+    output/s:
+    - test_scan: Output image containing the probability output segmentation
+    - if save_nifti: saves a nii file at the specified location
     """
-    if options['hostname'].startswith("hamlet"):
-        # batch_size = 2200
-        batch_size = 5120
-        options['batch_size'] = 350000
-    else:
-        # batch_size = 2800
-        batch_size = 2000
-
     # get_scan name and create an empty nii image to store segmentation
     tmp = {}
     tmp[scan] = test_x_data
@@ -45,38 +37,33 @@ def test_scan_uncertainty(model, test_x_data, scan, options, intermediate=None, 
 
     # get test paths
     _, scan = os.path.split(flair_scans[0])
-    test_folder = os.path.join('/host/silius/local_raid/ravnoor/01_Projects/55_Bayesian_DeepLesion_LoSo/data/predictions', options['experiment'])
-    # test_folder = '/host/silius/local_raid/ravnoor/01_Projects/06_DeepLesion_LoSo/data/predictions
+    test_folder = options['pred_folder']
     if not os.path.exists(test_folder):
-        # os.path.join(test_folder, options['experiment'])
         os.mkdir(test_folder)
 
     print('-'*60)
     print(str.replace(scan, '_flair.nii.gz', ''))
     print('-'*60)
-    # compute lesion segmentation in batches of size options['batch_size']
 
+    # compute lesion segmentation in batches of size options['batch_size']
     pred_fname = os.path.join(test_folder, str.replace(scan, '_flair.nii.gz', '') + '_out_pred_mean_0.nii.gz')
     if os.path.isfile(pred_fname):
         print("reading {} from disk".format(pred_fname))
         thresh_image = load_nii(pred_fname).get_data()
-        # print(thresh_image.shape, np.max(thresh_image))
     else:
         seg_image = np.zeros_like(flair_image)
         var_image = np.zeros_like(flair_image)
         thresh_image = np.zeros_like(flair_image)
         for batch, centers in load_test_patches(test_x_data, options, options['patch_size'], options['batch_size'], options['min_th'], candidate_mask):
             print("predicting uncertainty")
-            y_pred, y_pred_var = predict_uncertainty(model, batch, batch_size=batch_size, T=T)
+            y_pred, y_pred_var = predict_uncertainty(model, batch, batch_size=options['mini_batch_size'], T=T)
             [x, y, z] = np.stack(centers, axis=1)
             seg_image[x, y, z] = y_pred[:, 1]
             var_image[x, y, z] = y_pred_var[:, 1]
 
         if intermediate is not None:
-            # test_folder = str.replace(test_folder, 'brain', 'predictions')
             if not os.path.exists(test_folder):
                 os.mkdir(test_folder)
-            # out_scan = nib.Nifti1Image(seg_image, np.eye(4))
             out_scan = nib.Nifti1Image(seg_image, affine, header)
             test_name = str.replace(scan, '_flair.nii.gz', '') + '_out_pred_mean_0.nii.gz'
             out_scan.to_filename(os.path.join(test_folder, test_name))
@@ -85,12 +72,10 @@ def test_scan_uncertainty(model, test_x_data, scan, options, intermediate=None, 
             test_name = str.replace(scan, '_flair.nii.gz', '') + '_out_pred_var_0.nii.gz'
             out_scan.to_filename(os.path.join(test_folder, test_name))
 
-            # test_folder = str.replace(test_folder, 'brain', 'predictions')
             if not os.path.exists(os.path.join(test_folder, options['experiment'])):
                 os.mkdir(os.path.join(test_folder, options['experiment']))
 
             out_scan = nib.Nifti1Image(seg_image, affine, header)
-            #out_scan.to_filename(os.path.join(options['test_folder'], options['test_scan'], options['experiment'], options['test_name']))
             test_name = str.replace(scan, '_flair.nii.gz', '') + '_out_pred_0.nii.gz'
             out_scan.to_filename(os.path.join(test_folder, test_name))
 
@@ -101,12 +86,11 @@ def test_scan_uncertainty(model, test_x_data, scan, options, intermediate=None, 
 
 def select_voxels_from_previous_model(model, train_x_data, options):
     """
-    Select training voxels from image segmentation masks
+    select training voxels from image segmentation masks
     """
     threshold = options['th_dnn_train_2']
     # get_scan names
     scans = list(train_x_data.keys())
-    # mask  = [test_scan_uncertainty(model, dict(train_x_data.items()[s:s+1]), options, intermediate=1, uncertainty=True)[0] > threshold for s in trange(len(scans), desc='sel_vox_prev_model_pred_mean')]
     mask  = [test_scan_uncertainty(model, dict(train_x_data[scans[s]]), scans[s], options, intermediate=1, uncertainty=True) > threshold for s in trange(len(scans), desc='sel_vox_prev_model_pred_mean')]
 
     return mask
@@ -115,7 +99,7 @@ def select_voxels_from_previous_model(model, train_x_data, options):
 def predict_uncertainty(model, data, batch_size, T=10):
     input = model.layers[0].input
     output = model.layers[-1].output
-    f_stochastic = K.function([input, K.learning_phase()], output) # instantiates a Keras function.
+    f_stochastic = K.function([input, K.learning_phase()], output) # instantiate a Keras function.
     K.set_image_dim_ordering('th')
     K.set_image_data_format('channels_first')
 
@@ -128,7 +112,7 @@ def predict_uncertainty(model, data, batch_size, T=10):
 
 def predict_stochastic(f, ins, batch_size=128, verbose=False):
     '''
-        function to loop over some data in batches.
+        loop over some data in batches.
     '''
     nb_sample = len(ins)
     outs = []
@@ -156,29 +140,24 @@ def predict_stochastic(f, ins, batch_size=128, verbose=False):
 
 def load_training_data(train_x_data, train_y_data, options, subcort_masks, model=None):
     '''
-    Load training and label samples for all given scans and modalities.
+    load training and labels for all scans and modalities
 
-    Inputs:
-
-    train_x_data: a nested dictionary containing training image paths:
-        train_x_data['scan_name']['modality'] = path_to_image_modalities
-
-    train_y_data: a dictionary containing labels
-        train_y_data['scan_name'] = path_to_labels
-
-    options: dictionary containing general hyper-parameters:
-        - options['min_th'] = min threshold to remove voxels for training
+    inputs:
+    - train_x_data: a nested dictionary containing training image paths:
+        - train_x_data['scan_name']['modality'] = path_to_image_modalities
+    - train_y_data: a dictionary containing labels
+        - train_y_data['scan_name'] = path_to_labels
+    - options: dictionary containing general hyper-parameters:
+        - options['min_th'] = minimum threshold to remove voxels for training
         - options['size'] = tuple containing patch size, 3D (p1, p2, p3)
-        - options['randomize_train'] = randomizes data
+        - options['randomize_train'] = randomize data
+    - model: CNN model used to select training candidates
 
-    model: CNN model used to select training candidates
-
-    Outputs:
+    outputs:
         - X: np.array [num_samples, num_channels, p1, p2, p2]
         - Y: np.array [num_samples, 1]
 
     '''
-
     # get_scan names and number of modalities used
     scans = list(train_x_data.keys())
     modalities = train_x_data[scans[0]].keys()
@@ -295,7 +274,7 @@ def load_train_patches(x_data, y_data, selected_voxels, patch_size, subcort_mask
     else:
         nolesion_masks = [np.logical_and(np.logical_not(binary_dilation(lesion, iterations=5)), brain) for lesion, brain in zip(lesion_masks, selected_voxels)]
 
-    # Get all the x,y,z coordinates for each image
+    # retrieve x,y,z coordinates for each image
     lesion_centers = [get_mask_voxels(mask) for mask in lesion_masks]
     nolesion_centers = [get_mask_voxels(mask) for mask in nolesion_masks]
 
@@ -323,7 +302,6 @@ def binarize_label_gm(mask):
     mask_ = np.zeros_like(mask)
     tmp = np.stack(np.where(mask == 1), axis=1)
     mask_[tmp[:,0], tmp[:,1], tmp[:,2]] = 1
-    # print("mask shape: {}, type: {}, unique: {}".format(mask_.shape, type(mask_), np.unique(mask_)))
     return mask_.astype(np.bool)
 
 
